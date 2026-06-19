d4d0ecea0a1a09906369fa58ec2036d12af0e463
sanofer07 <lilyglow1234@gmail.com>
2026-06-19 22:08:13 +0530

    Fix API base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({
+  baseURL: 'https://school-complaint-suggestion-portal.onrender.com/api'
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
